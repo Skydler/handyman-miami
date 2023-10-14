6d95cf100afc7ad3cc7324387a948108d57345ec
Leonel Mandarino <leonelmandarino@gmail.com>
2023-10-14 19:44:54 -0300

    Add recent works section

---
+++ components/works/card.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  img: String,
+  title: String,
+});
+</script>
+
+<template>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg mb-5">
+    <NuxtImg
+      class="w-full"
+      :src="props.img"
+      :alt="props.title"
+      width="400"
+      height="600"
+      fit="cover"
+    />
+    <div class="px-6 py-2 text-center">
+      <p class="text-gray-700 text-base">{{ props.title }}</p>
+    </div>
+  </div>
+</template>
