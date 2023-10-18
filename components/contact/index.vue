ffa87f9f2b04fd7590865301cfc73f2ac2ca3bd0
GabyAragon <Gabriel_parki98@hotmail.com>
2023-10-17 22:15:56 -0300

    Ayuda con el contact

--- components/contact/index.vue
+++ components/contact/index.vue
@@ -1,86 +1,98 @@
 <template>
-    <section class="bg-[#282828]" id="contact">
+  <section>
+    <div class="bg-[#282828]" id="contact">
       <div class="mx-auto max-w-7xl p-10">
-        <div class="">
-          <div class="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
-            <h2 class="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
-              Contact Us!
-            </h2>
-          </div>
+        <div class="max-w-4xl text-center sm:text-center md:mx-auto">
+          <h2 class="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
+            Contact Us!
+          </h2>
+          <p class="mt-3 text-xl text-gray-100">
+            Got a home improvement project that needs expert attention? We're here to help! Our team of skilled handymen is a phone call away. Reach out to us today, and let's get started on making your home the best it can be.
+          </p>
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-          <div>
-            <p class="mt-3 text-lg text-gray-100">
-              Got a home improvement project that needs expert attention? We're here to help! Our team of skilled handymen is a phone call away. Reach out to us today, and let's get started on making your home the best it can be.
-            </p>
-            <ul class="mt-6 grid grid-cols-1 gap-4">
-              <li class="flex items-center space-x-4">
-                <div class="w-10 h-10 bg-gray-100 text-gray-900 rounded-full flex items-center justify-center">
-                  <!-- Icon for address -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
-                    <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
-                    <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
-                  </svg>
+      </div>
+    </div>
+    <div class="grid grid-cols-1 xl:grid-cols-3 gap-8 items-center"> 
+      <!-- Columna 1-->
+      <div class="grid grid-cols-1 xl:grid-cols-2 col-span-2 items-center card bg-[#282828] m-5 rounded p-5 min-h-[95%] max-h-[95%]">
+        <div>
+          <h2 class="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
+            Contact Information
+          </h2>
+          <ul class="mt-6 grid grid-cols-1">
+            <li class="flex items-center space-x-4">
+              <div class="w-10 h-10 bg-gray-100 text-gray-900 rounded-full flex items-center justify-center">
+                <!-- Icon for address -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
+                  <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                  <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
+                </svg>
+              </div>
+              <div>
+                <h3 class="text-lg font-medium text-gray-100">Location</h3>
+                <p class="text-gray-100"> We offer our services throughout the state of Florida </p>
+              </div>
+            </li>
+            <li class="flex items-center space-x-4">
+              <div class="w-10 h-10 bg-gray-100 text-gray-900 rounded-full flex items-center justify-center">
+                <!-- Icon for contact -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
+                  <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
+                  <path d="M15 7a2 2 0 0 1 2 2"></path>
+                  <path d="M15 3a6 6 0 0 1 6 6"></path>
+                </svg>
+              </div>
+              <div>
+                <h3 class="text-lg font-medium text-gray-900 dark:text-white">Contact</h3>
+                <div class="flex">
+                  <p class="text-gray-100">(786) 634-8023 &nbsp</p>
                 </div>
-                <div>
-                  <h3 class="text-lg font-medium text-gray-900 dark:text-white">Location</h3>
-                  <p class="text-gray-100"> We offer our services throughout the state of Florida </p>
-                </div>
-              </li>
-              <li class="flex items-center space-x-4">
-                <div class="w-10 h-10 bg-gray-100 text-gray-900 rounded-full flex items-center justify-center">
-                  <!-- Icon for contact -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
-                    <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
-                    <path d="M15 7a2 2 0 0 1 2 2"></path>
-                    <path d="M15 3a6 6 0 0 1 6 6"></path>
-                  </svg>
-                </div>
-                <div>
-                  <h3 class="text-lg font-medium text-gray-900 dark:text-white">Contact</h3>
-                  <div class="flex">
-                    <p class="text-gray-100">(786) 634-8023 &nbsp</p>
-                  </div>
-                  <div class="flex">
-                    <p class="text-gray-100">(206) 636-2456 &nbsp </p>
-                  </div> 
-                </div>
-                
-              </li>
-              <a class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 flex md:w-1/3" href="https://wa.me/7866348023"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-whatsapp" viewBox="0 0 16 16"><path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>  </svg> &nbsp Chat with Us! </a>
-              <li class="flex items-center space-x-4">
-                <div class="w-10 h-10 bg-gray-100 text-gray-900 rounded-full flex items-center justify-center">
-                  <!-- Icon for Email -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/> </svg>
-                </div>
-                <div>
-                  <h3 class="text-lg font-medium text-gray-900 dark:text-white">E-Mail</h3>
-                  <p class="text-gray-100">Handymanteam28@gmail.com</p>
-                </div>
-              </li>
-              <li class="flex items-center space-x-4">
-                <div class="w-10 h-10 bg-gray-100 text-gray-900 rounded-full flex items-center justify-center">
-                  <!-- Icon for working hours -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
-                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
-                    <path d="M12 7v5l3 3"></path>
-                  </svg>
-                </div>
-                <div>
-                  <h3 class="text-lg font-medium text-gray-900 dark:text-white">Working hours</h3>
-                  <p class="text-gray-100">Monday - Friday: 08:00 - 17:00</p>
-                  <p class="text-gray-100">Saturday &amp; Sunday: 08:00 - 12:00</p>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <div class="card bg-gray-100 rounded">
-              <iframe class="rounded p-2 w-full mt-2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7241652.012888893!2d-83.804601!3d27.5946799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c1766591562abf%3A0xf72e13d35bc74ed0!2sFlorida%2C%20USA!5e0!3m2!1sen!2sar!4v1697573793765!5m2!1sen!2sar" width="300" height="350" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
+                <div class="flex">
+                  <p class="text-gray-100">(206) 636-2456 &nbsp </p>
+                </div> 
+              </div>
+              
+            </li>
+            
+            <li class="flex items-center space-x-4">
+              <div class="w-10 h-10 bg-gray-100 text-gray-900 rounded-full flex items-center justify-center">
+                <!-- Icon for Email -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/> </svg>
+              </div>
+              <div>
+                <h3 class="text-lg font-medium text-gray-900 dark:text-white">E-Mail</h3>
+                <p class="text-gray-100">Handymanteam28@gmail.com</p>
+              </div>
+            </li>
+            <li class="flex items-center space-x-4">
+              <div class="w-10 h-10 bg-gray-100 text-gray-900 rounded-full flex items-center justify-center">
+                <!-- Icon for working hours -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
+                  <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                  <path d="M12 7v5l3 3"></path>
+                </svg>
+              </div>
+              <div>
+                <h3 class="text-lg font-medium text-gray-900 dark:text-white">Working hours</h3>
+                <p class="text-gray-100">Monday - Friday: 08:00 - 17:00</p>
+                <p class="text-gray-100">Saturday &amp; Sunday: 08:00 - 12:00</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="mt-5 md:mt-0">
+          <h2 class="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl ">
+              Whatsapp
+          </h2>
+          <div class="flex-col max-w-xs">
+            <img class="hidden xl:block rounded" src="resources/wasa.png" alt="Qr Whatsapp"/>
+            <a class="mt-5 focus:outline-none text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-8 py-2.5 mr-2 mb-2 flex" href="https://wa.me/7866348023"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-whatsapp mt-1" viewBox="0 0 16 16"><path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>  </svg> <p class="text-xl">&nbsp Chat with Us!</p> </a>
           </div>
         </div>
       </div>
-    </section>
-  </template>
+      
+      
+    </div>
+  </section>
+</template>
   
